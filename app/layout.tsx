151bf2277adfe0dfe81a66a87c03a6e1516ece91
Nick Fausti <ncfausti@gmail.com>
2023-08-30 21:18:33 -0400

    BH-DEMO Add recharts and multi-axis chart with zoom

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
                 <code className='font-mono font-bold'>API</code>
               </Link>
             </p>
-
             <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'></div>
           </div>
           {children}
